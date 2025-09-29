4fc16d05c06f37fe3b9dcc97bd9db1fb8f68bab1
1Hunor4 <c2verhun@vasvari.org>
2025-09-29 09:19:36 +0200

    Téglalap terület, kerület kész

---
+++ feladat2.py
@@ -0,0 +1,6 @@
+print("A téglalap területének kiszámítása:")
+a = int(input("Adja meg a téglalap egyik oldalát:"))
+b = int(input("Adja meg a téglalap másik oldalát:"))
+
+print("A téglalap területe:", a*b)
+print("A téglalap kerülete:", 2*(a+b))
